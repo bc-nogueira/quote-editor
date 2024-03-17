41700ed0c76b2d3aa5616b87555d57cd8da994e0
Breno Nogueira <brenocnog@gmail.com>
2024-03-16 23:30:42 -0300

    Chapter 11

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,6 +1,7 @@
 class Quote < ApplicationRecord
   belongs_to :company
   has_many :line_item_dates, dependent: :destroy
+  has_many :line_items, through: :line_item_dates
 
   validates :name, presence: true
 
@@ -10,6 +11,9 @@ class Quote < ApplicationRecord
   # Those three callbacks are equivalent to the following single line
   broadcasts_to ->(quote) { [quote.company, 'quotes'] }, inserts_by: :prepend
 
-
   scope :ordered, -> { order(id: :desc) }
+
+  def total_price
+    line_items.sum(&:total_price)
+  end
 end
